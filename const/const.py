fdb8fe07be19e12b67bffd774bdda4222c8b7fb8
mahyar karimi <mahyar.karimi@divar.ir>
2022-06-03 20:07:27 +0430

    add const/

--- const/const.py
+++ const/const.py
@@ -1,7 +1,12 @@
 from nutellamd_pb2 import Address
 
+
 class Const:
     MASTER_ADDRESS = Address(
         ip=1,
         port=54321
     )
+
+    class Peer:
+        GET_NEIGHBOUR_RATE = 5
+        IDENTIFIER_LENGTH = 10
