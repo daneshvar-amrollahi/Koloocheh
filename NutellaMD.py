93bd3d7b28b59f04d3b980701fab32c325bf0c47
mahyar karimi <mahyar.karimi@divar.ir>
2022-06-03 20:10:08 +0430

    refactor NutellaMD.py

--- NutellaMD.py
+++ NutellaMD.py
@@ -24,7 +24,7 @@ def main():
                 port=port
             )
             peer_interface = PeerInterface(address)
-            peer_interface.advertise_to_master(address)
+            peer_interface.advertise_to_master()
             peer_interface.run()
 
 
